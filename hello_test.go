f5892c7db48fe53a800d627a692b319cebf08652
Eric Petry <Eric.Petry@mail.schwarz>
2021-09-09 11:05:14 +0200

    refactoring code and test

--- hello_test.go
+++ hello_test.go
@@ -4,11 +4,28 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
-	got := Hello(" Eric")
-	want := "Hello, Eric"
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		if got != want {
 
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Eric")
+		want := "Hello, Eric"
+
+		assertCorrectMessage(t, got, want)
+
+	})
+
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
